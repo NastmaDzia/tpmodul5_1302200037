38308394ffd0a4aa70cb9ce0463112bc19a56066
NastmaDzia <100849580+NastmaDzia@users.noreply.github.com>
2022-03-25 15:57:35 +0700

    menambahkan class SayaTubeVideo

--- tpmodul5_1302200037/Program.cs
+++ tpmodul5_1302200037/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace tpmodul5_1302200037
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo vidio = new SayaTubeVideo("Tutorial Design By Contract - Nastma Dzia Ulhaque");
+            vidio.PrintVideoDetails();
+
+            vidio.IncreasePlayCount(69);
+            vidio.PrintVideoDetails();
+
+            try
+            {
+                vidio.IncreasePlayCount(1900000000);
+                vidio.PrintVideoDetails();
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ tpmodul5_1302200037/SayaTubeVideo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace tpmodul5_1302200037
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo() { }
+
+        public SayaTubeVideo(string title)
+        {
+            Contract.Requires(title != null);
+            Contract.Requires(title.Length <= 100);
+            this.title = title;
+
+            var rand = new Random();
+            this.id = rand.Next(10000, 99999);
+
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int a)
+        {
+            Contract.Requires(a <= 1000000);
+            try
+            {
+                if (a > 10000000) throw new Exception("Jumlah input play count melebihi 10.000.000!");
+                this.playCount = checked(this.playCount + a);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID\t: {0}", this.id);
+            Console.WriteLine("Video Title\t: {0}", this.title);
+            Console.WriteLine("Video Playcount\t: {0}\n", this.playCount);
+
+        }
+    }
+}
